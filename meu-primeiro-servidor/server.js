c6cb61228ae5c64396d52d6fd2c262b1743814c9
Lucilania <lucilaniasilva@yahoo.com.br>
2022-05-20 20:15:00 -0300

    atualizacao

---
+++ meu-primeiro-servidor/server.js
@@ -0,0 +1,19 @@
+// const { response } = require("express")
+const express = require("express")    //invocação//
+const app   = express()   //excecução//
+
+//rota//
+app.get("/", (request, response)=>{
+    response.status(200).json(["salve, mundao"])
+
+})
+app.get("/ola", (request, response)=>{
+    response.status(200).json(["oieee"])
+})
+
+//configurar porta//
+app.listen(8082, ()=>{    //criação da porta
+    console.log("Meu servidor ta rodadando na porta 8080!!)
+
+})
+
